354b976006e7789eda504416698d1aa760fba997
Daniel Wilkins <danielwilkins5@gmail.com>
2014-07-11 15:40:15 +0100

    * First Commit

---
+++ Assets/Ad Networks Prefabs/ChartboostAdController.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using Chartboost;
+
+public class ChartboostAdController : MonoBehaviour {
+
+	static GameObject chartboostAdBannerObject;	
+	
+	public string appID;
+	public string appSignature;
+
+	void Awake()
+	{
+		//check if chartboostAdBannerObject Exits
+		if(chartboostAdBannerObject)
+		{
+			//it does so destory this object
+			Destroy(gameObject);
+		}
+		else
+		{
+			//else make set chartboostAdBannerObject to this object and dont destroy on load
+			chartboostAdBannerObject = gameObject;
+			DontDestroyOnLoad(gameObject);
+		}	
+	}
+	
+	void Update(){
+		if (Input.GetKeyUp(KeyCode.Escape)) {
+			if (CBBinding.onBackPressed())
+				return;
+		}
+	}
+
+	void OnEnable() {
+		CBBinding.init(appID, appSignature);
+		CBBinding.cacheInterstitial(null);
+		CBBinding.cacheMoreApps();
+	}
+
+	void OnApplicationPause(bool paused) {
+		// Manage Chartboost plugin lifecycle
+		CBBinding.pause(paused);
+	}
+	
+	void OnDisable() {
+		// Shut down the Chartboost plugin
+		CBBinding.destroy();
+	}
+}
