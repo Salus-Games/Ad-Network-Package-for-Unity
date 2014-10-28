0f8d87768bd47a2515ed9e80bf11750de8622296
Daniel Wilkins <danielwilkins5@gmail.com>
2014-10-28 16:21:57 +0000

    * Changed the GoogleAdsController script for more easiness

--- Assets/GoogleMobileAds/GoogleAdController.cs
+++ Assets/GoogleMobileAds/GoogleAdController.cs
@@ -6,9 +6,26 @@ public class GoogleAdController : MonoBehaviour {
 
 	static GameObject googleAdBannerObject;	
 
-	public string adUnitId;
-	public bool adTop;
-	public bool adBottom; 
+	public string googleAdID;
+    public string amazonAdID;
+    public string iosAdID;
+    private string selectedAdID;
+
+    public enum platforms
+    {
+        Google,
+        Amazon,
+        iOS,
+    }
+    public platforms targetPlatform;
+
+    public enum adLocations
+    {
+        Top,
+        Bottom,
+    }
+    public adLocations adLocation;
+    private GoogleMobileAds.Api.AdPosition adPostion;
 
 	private static BannerView bannerView;
 
@@ -25,32 +42,43 @@ public class GoogleAdController : MonoBehaviour {
 			//else make set GoogleAdbannerObject to this object and dont destroy on load
 			googleAdBannerObject = gameObject;
 			DontDestroyOnLoad(gameObject);
-		}	
+		}
+
+        switch(targetPlatform)
+        {
+            case platforms.Google:
+                selectedAdID = googleAdID;
+                break;
+            case platforms.Amazon:
+                selectedAdID = amazonAdID;
+                break;
+            case platforms.iOS:
+                selectedAdID = iosAdID;
+                break;
+        }
+
+        switch(adLocation)
+        {
+            case adLocations.Top:
+                adPostion = AdPosition.Top;
+                break;
+            case adLocations.Bottom:
+                adPostion = AdPosition.Bottom;
+                break;
+        }
+
 	}
 
 
 	// Use this for initialization
 	void Start () 
 	{	
-		
-		if(adTop)
-		{
-			// Create a banner
-			bannerView = new BannerView(adUnitId, AdSize.SmartBanner, AdPosition.Top);
-			// Create an empty ad request.
-			AdRequest request = new AdRequest.Builder().Build();
-			// Load the banner with the request.
-			bannerView.LoadAd(request);
-		}
-		if(adBottom)
-		{
-			// Create a banner
-			bannerView = new BannerView(adUnitId, AdSize.SmartBanner, AdPosition.Bottom);
-			// Create an empty ad request.
-			AdRequest request = new AdRequest.Builder().Build();
-			// Load the banner with the request.
-			bannerView.LoadAd(request);
-		}
+	    // Create a banner
+        bannerView = new BannerView(selectedAdID, AdSize.SmartBanner, adPostion);
+		// Create an empty ad request.
+		AdRequest request = new AdRequest.Builder().Build();
+		// Load the banner with the request.
+		bannerView.LoadAd(request);
 	}
 
 	public static void HideBanner()
--- README.md
+++ README.md
@@ -3,9 +3,9 @@ Ad-Network-Package-for-Unity
 
 This is a simple Unity Package to get AdMob and Chartboost ads into your Apps.
 
-1. Just drag the 2 prefabs in the Ad Networks Prefabs folder into your scene
-2. Fill out the relevent App Id's and signatures on the scripts attached to the prefabs
-3. Check the Demo scene and DemoCUI.cs script to see how to call a Chartbost ad or to show and hide the AdMob banners
+1. Drag the Chartboost prefab in the Chartboost folder and the Google Ads prefab in the GoogleMobileAds folder into your first scene
+2. Fill out the fields on the Google Ads prefab and click Chartboost -> Edit Settings and fill out the Chartboost info.
+3. For a example go to the Examples folder and open the ChartboostExample scene.
 
 ###Android###
 No more steps needed to work
